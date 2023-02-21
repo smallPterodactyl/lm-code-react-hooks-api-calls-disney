d07e11c77efdd60b5c0246ca60eae1c0cff85b9a
smallPterodactyl <119345529+smallPterodactyl@users.noreply.github.com>
2023-02-21 13:58:48 +0000

    Add toggle and OnClick listener

--- src/components/character.tsx
+++ src/components/character.tsx
@@ -10,28 +10,51 @@ interface CharacterProps {
 // for our props we can reuse the DisneyCharacter interface
 // - defining an anonymous type that just has one property - a DisneyCharacter
 const Character : React.FC<CharacterProps> = ( { character,
-                                                   characterFavorites,
-                                                   updateFavorites } ) => {
+                                                  characterFavorites,
+                                                  updateFavorites } ) => {
+
+                                                    
   
   let imageSrc = "https://picsum.photos/300/200/?blur";   
 
   if (character.imageUrl) {imageSrc = character.imageUrl};
 
-  return (
+  function toggleFavoriteForCharacter (characterID: number) {
+
+    let updatedFavorites : Array<number> = [];
+
+    //add a new favorite
+    if (!characterFavorites.includes(characterID)) {
+      updateFavorites ([...characterFavorites,characterID]);
+    }
+
+    //filter out a removed favorite
+    else {
+      updatedFavorites = 
+      characterFavorites.filter((id) => id !== characterID);
+
+      updateFavorites(updatedFavorites);
+    }  
+      
+  } 
+
+  {return (
 
     <article className="character-item">
 
       <h2>{character.name}</h2>
 
-      <div className="character-item__actions">
+      <div className="character-item__actions"
+       onClick={()=>
+        toggleFavoriteForCharacter(character._id)}>
         Add to Favourites
       </div>
 
       <img className="character-item__img" src={character.imageUrl} alt={character.name} />
 
     </article>
-  )  
-
+  )  }
+  
 }  
   
 export default Character
